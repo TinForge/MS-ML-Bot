3f03eb567331dadec4a3304fb5b90aae1eee762d
TinForge <29712552+TinForge@users.noreply.github.com>
2023-01-10 22:25:56 -0500

    Changed method for refreshing labels

--- tkinter/mainpage.py
+++ tkinter/mainpage.py
@@ -3,45 +3,64 @@ from tkinter.ttk import *
 import PIL.Image
 import PIL.ImageTk
 import window
-import startpage
-import references
+import overlay
 
-overlay_active = False
+overlay_visible = False
+maplestory_found = False
+
 
 
 class MainPage(Frame):
     def __init__(self, app: window.Window):
-        app.tk.geometry("300x450")
+        self.app = app
+        self.app.tk.geometry("300x450")
         Frame.__init__(self, app.tk)
         Label(self, text="Main Page", font='bold').pack(pady=10)
         Button(self, text="Toggle Overlay", command=lambda: self.toggle_overlay()).pack()
-        OverlayPanel(self)
-        StatusPanel(self)
+        self.overlayPanel = OverlayPanel(self)
+        self.statusPanel = StatusPanel(self)
+        self.overlay1 = None
 
     def toggle_overlay(self):
-        global overlay_active
-        overlay_active = not overlay_active
-        print("Toggled Overlay to " + str(overlay_active))
-        window.instance.switch_page(MainPage)
+        global overlay_visible
+        overlay_visible = not overlay_visible
+        if overlay_visible:
+            self.overlay1 = overlay.Overlay(self.app.tk)
+        else:
+            self.overlay1.frame.destroy()
+
+        self.overlayPanel.refresh()
+        self.statusPanel.refresh()
 
 
 class OverlayPanel(LabelFrame):
     def __init__(self, frame):
-        global overlay_active
         LabelFrame.__init__(self, frame, text="Overlay", borderwidth=1, relief=GROOVE)
-        Label(self, text="Overlay Display:").grid(row=1, column=0, padx=10, pady=10)
-        Label(self, text=overlay_active).grid(row=1, column=1, padx=10, pady=10)
+        Label(self, text="Overlay Visible:").grid(row=1, column=0, padx=10, pady=10)
+        self.overlay_visible_text = Label(self)
+        self.overlay_visible_text.grid(row=1, column=1, padx=10, pady=10)
         self.grid_columnconfigure((0, 1), weight=1, uniform="column")
         self.pack(fill=X)
+        self.refresh()
+
+    def refresh(self):
+        global overlay_visible
+        self.overlay_visible_text['text'] = overlay_visible
 
 
 class StatusPanel(LabelFrame):
     def __init__(self, frame):
         LabelFrame.__init__(self, frame, text="Status", borderwidth=1, relief=GROOVE)
         Label(self, text="Maplestory Found:").grid(row=0, column=0, padx=10, pady=10)
-        Label(self, text="No").grid(row=0, column=1, padx=10, pady=10)
+        self.maplestory_found_text = Label(self)
+        self.maplestory_found_text.grid(row=0, column=1, padx=10, pady=10)
         self.grid_columnconfigure((0, 1), weight=1, uniform="column")
         self.pack(fill=X)
+        self.refresh()
+
+    def refresh(self):
+        global maplestory_found
+        self.maplestory_found_text['text'] = maplestory_found
 
 
 def main():
