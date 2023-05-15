79e3d391e86feb2f6aed18d3c0420b1e2676e2cf
TinForge <29712552+TinForge@users.noreply.github.com>
2023-05-15 18:51:12 -0400

    Merged changes
    forgot to pull, oops

--- gui/overlay.py
+++ gui/overlay.py
@@ -8,20 +8,21 @@ from mapletools import tools
 is_visible = False
 
 
+# Data container for rect bounds
 class Rect:
-    def __init__(self, x1, y1, x2, y2):
+    def __init__(self, x1, y1, x2, y2, color):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+        self.color = color
 
 
-test_rects = [Rect(100, 100, 200, 200), Rect(300, 300, 400, 400), Rect(500, 500, 600, 600)]
+# Test set of rects
+test_rects = [Rect(100, 100, 200, 200, "green"), Rect(300, 300, 400, 400, "red"), Rect(500, 500, 600, 600, "purple")]
 
 
 class Overlay:
-
-
     def __init__(self, instance: Tk):
         self.instance = instance
         self.frame = Toplevel()
@@ -39,21 +40,16 @@ class Overlay:
         #
         self.canvas = Canvas(self.frame, bg='#add123', bd=0, highlightthickness=0)
         self.canvas.config(width=self.width, height=self.height)  # fill screen
-
-        # WIP
+        #
         self.shapes = set()  # declare data structure
-
+        #
         self.display_rects(test_rects)
 
 
-
-
-    def display_rects(self, new_rects: list):  # rects needs to be a list of rects
+    def display_rects(self, new_rects: list):
         self.rects = new_rects
-        # print(self.rects)
         self.clear_shapes()
         self.render_shapes()
-
 
     def clear_shapes(self):
         for s in self.shapes:
@@ -63,15 +59,9 @@ class Overlay:
     def render_shapes(self):
         r: Rect
         for r in self.rects:
-            self.shapes.add(self.canvas.create_rectangle(r.x1, r.y1, r.x2, r.y2, fill='', outline='green', width=2))
+            self.shapes.add(self.canvas.create_rectangle(r.x1, r.y1, r.x2, r.y2, fill='', outline=r.color, width=2))
         self.canvas.pack()
 
-
-
-        # (x1,y1) top left corner and (x2, y2) bottom right corner
-        # self.canvas.create_rectangle(50, 110, 300, 280, fill='', outline='red', width=2)
-        # self.canvas.create_rectangle(0, 0, 222, 155, fill='', outline='blue', width=2)
-        # self.canvas.pack()
 
 
 class OverlayTester(Frame):
@@ -86,50 +76,6 @@ class OverlayTester(Frame):
 
     def close_window(self):
         self.overlay.frame.destroy()
-
-class OverlayTester(Frame):
-    def __init__(self, app: window.Window):
-        self.instance = app
-        Frame.__init__(self, app.tk)
-        Button(self, text="Enable Overlay", command=self.new_window).pack()
-        Button(self, text="Disable Overlay", command=self.close_window).pack()
-
-    def new_window(self):
-        self.overlay = Overlay(self.instance.tk)
-
-        self.width = self.instance.winfo_screenwidth()
-        self.height = self.instance.winfo_screenheight()
-
-        self.frame.geometry("%dx%d" % (self.width, self.height))
-        self.frame.title("Overlay")
-        self.frame.attributes('-fullscreen', True)
-        self.frame.config(bg='#add123')  # Makes background transparent
-        self.frame.wm_attributes('-transparentcolor', '#add123')
-        self.frame.wm_attributes("-topmost", True)
-        self.frame.wm_attributes("-disabled", True)
-
-        self.canvas = Canvas(self.frame, bg='#add123', bd=0, highlightthickness=0)
-        self.canvas.config(width=self.width, height=self.height)  # fill screen
-
-        self.rects = set()
-        self.shapes = set()
-
-
-    def refresh(self):
-        for s in self.shapes:
-            self.canvas.delete(s)
-        self.shapes.clear()
-
-        for s in self.rects:
-            rect = self.canvas.create_rectangle(50, 110, 300, 280, fill='', outline='red', width=2)
-            self.shapes.add(rect)
-
-        # (x1,y1) top left corner and (x2, y2) bottom right corner
-        self.canvas.create_rectangle(50, 110, 300, 280, fill='', outline='red', width=2)
-        self.canvas.create_rectangle(0, 0, 222, 155, fill='', outline='blue', width=2)
-        # self.canvas.pack()
-
-
 
 
 def main():
