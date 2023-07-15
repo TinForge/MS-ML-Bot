97c2a37730225f0cc24c9e715d765e8f90b75251
TinForge <29712552+TinForge@users.noreply.github.com>
2023-07-15 00:01:04 -0400

    Added persistent input field

--- gui/mainpage.py
+++ gui/mainpage.py
@@ -10,6 +10,7 @@ import window
 import overlay
 #
 from mapletools import values, tools
+from windowtools import savedata
 
 
 # Frame layout for main page
@@ -69,14 +70,19 @@ class StatusPanel(LabelFrame):
     def __init__(self, frame):
         LabelFrame.__init__(self, frame, text="Status", borderwidth=1, relief=GROOVE)
         #
-
+        self.window_name = Entry(self, textvariable=savedata.window_name.read(), font=references.var_font, width=30)
+        self.window_name.grid(row=0, column=0, padx=15, pady=5, columnspan=2)
+        self.window_name.bind('<KeyRelease>', self.on_window_name_change)
+        self.window_name.insert(0, savedata.window_name.read())
+        tools.windowName = self.window_name.get()
+        #
         self.window_found = Label(self, image=references.red_icon)
-        self.window_found.grid(row=0, column=0, padx=15, pady=5)
-        Label(self, text="Window Found", font=references.var_font).grid(sticky=W, row=0, column=1, padx=0, pady=0)
+        self.window_found.grid(row=1, column=0, padx=15, pady=5)
+        Label(self, text="Window Found", font=references.var_font).grid(sticky=W, row=1, column=1, padx=0, pady=0)
         #
         self.window_active = Label(self, image=references.red_icon)
-        self.window_active.grid(row=1, column=0, padx=15, pady=5)
-        Label(self, text="Window Active", font=references.var_font).grid(sticky=W, row=1, column=1, padx=0, pady=0)
+        self.window_active.grid(row=2, column=0, padx=15, pady=5)
+        Label(self, text="Window Active", font=references.var_font).grid(sticky=W, row=2, column=1, padx=0, pady=0)
         #
         self.grid_columnconfigure(1, weight=1, uniform="column")
         self.pack(fill=X)
@@ -86,6 +92,10 @@ class StatusPanel(LabelFrame):
         self.window_found.configure(image=(references.red_icon, references.green_icon)[values.isWindowFound])
         self.window_active.configure(image=(references.red_icon, references.green_icon)[values.isWindowActive])
         self.after(500, self.refresh)
+
+    def on_window_name_change(self, event):
+        savedata.window_name.save(self.window_name.get())
+        tools.windowName = self.window_name.get()
 
 
 # Panel layout for Test variables
---
+++ windowtools/savedata.py
@@ -0,0 +1,33 @@
+
+import os
+
+
+class TextFile():
+    def __init__(self, file_name, default_value):
+        self.file_path = './windowtools/' + file_name + '.txt'
+        if (self.read() == ''):
+            self.save(default_value)
+
+    def read(self):
+        if os.path.exists(self.file_path):
+            with open(self.file_path, 'r') as f:
+                return f.read()
+        else:
+            return ''
+
+
+    def save(self, value):
+        with open(self.file_path, 'w') as f:
+            f.write(value)
+
+
+window_name = TextFile('window name', 'Window Name')
+
+
+def main():
+    window_name.save('test')
+    print(window_name.read())
+
+
+if __name__ == "__main__":
+    main()
