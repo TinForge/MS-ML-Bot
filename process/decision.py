a1ae28235aca8bfc19eb251ceb4ace1d778d7740
TinForge <29712552+TinForge@users.noreply.github.com>
2023-10-26 00:21:10 -0400

    adjusting decision logic

--- process/decision.py
+++ process/decision.py
@@ -14,6 +14,7 @@ class DecisionThread(threading.Thread):
         instance = self
 
         self.is_running = False
+        self.key = keyboard.VK_MENU
 
 
     def run(self):
@@ -43,25 +44,37 @@ class DecisionThread(threading.Thread):
             x_distance = monster.center_x - player.center_x
             y_distance = monster.center_y - player.center_y
 
-            direction = "nothing"
+            action = "nothing"
 
             if x_distance < 0:
-                direction = "left"
-                keyboard.PressKey(keyboard.VK_LEFT)
-                time.sleep(0.2)
-                keyboard.ReleaseKey(keyboard.VK_LEFT)
+                action = "left"
             elif x_distance > 0:
-                direction = "right"
-                keyboard.PressKey(keyboard.VK_RIGHT)
-                time.sleep(0.2)
-                keyboard.ReleaseKey(keyboard.VK_RIGHT)
+                action = "right"
             if abs(x_distance) < 120:
-                direction = "attack"
-                keyboard.PressKey(keyboard.VK_CONTROL)
-                time.sleep(0.2)
-                keyboard.ReleaseKey(keyboard.VK_CONTROL)
+                action = "attack"
 
-            values.debug_action = direction
+            if action == "left" and self.key is not keyboard.VK_LEFT:
+                keyboard.ReleaseKey(self.key)
+                self.key = keyboard.VK_LEFT
+                keyboard.PressKey(self.key)
+
+            elif action == "right" and self.key is not keyboard.VK_RIGHT:
+                keyboard.ReleaseKey(self.key)
+                self.key = keyboard.VK_RIGHT
+                keyboard.PressKey(self.key)
+
+            elif action == "attack":
+                keyboard.ReleaseKey(self.key)
+                self.key = keyboard.VK_CONTROL
+                time.sleep(0.1)
+                keyboard.PressKey(self.key)
+
+            elif action == "nothing":
+                keyboard.ReleaseKey(self.key)
+                self.key = keyboard.VK_MENU
+
+
+            values.debug_action = action
             values.debug_distance = x_distance
 
 
